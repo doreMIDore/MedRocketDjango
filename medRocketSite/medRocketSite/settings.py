76cb81a3b5612318203b5dda339ed91ab9e08c30
doreMIDore <Igor12341999@gmail.com>
2023-04-18 16:24:40 +0300

    install postgresql db

--- medRocketSite/medRocketSite/settings.py
+++ medRocketSite/medRocketSite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'medRocketSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'medrocketdb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
